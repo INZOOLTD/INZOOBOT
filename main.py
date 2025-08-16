ecf63f74bf2f2bb7d304235e2c55a89508d25add
Echo <luorenhong@mail.inzoo.ltd>
2025-08-16 16:47:27 +0800

    测试V0.1.5

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ class MyPlugin(Star):
         super().__init__(context)
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("天馁在嘛")
+    @filter.command("天馁")
     async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+        '''关键词回复''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 获取消息的纯文本内容
         logger.info("触发hello world指令!")
